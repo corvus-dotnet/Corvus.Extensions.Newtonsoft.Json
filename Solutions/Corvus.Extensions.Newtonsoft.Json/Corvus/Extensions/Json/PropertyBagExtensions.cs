b3508622fc12a4f05ce8ace6512c2f285bc67b94
Matthew Adams <matthew.adams@endjin.com>
2019-09-22 22:17:24 +0100

    Added a useful extension to convert the PropertyBag into an IDictionary<string, object>

---
+++ Solutions/Corvus.Extensions.Newtonsoft.Json/Corvus/Extensions/Json/PropertyBagExtensions.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="PropertyBagExtensions.cs" company="Endjin Limited">
+// Copyright (c) Endjin Limited. All rights reserved.
+// </copyright>
+
+namespace Corvus.Extensions.Json
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Newtonsoft.Json.Linq;
+
+    /// <summary>
+    /// Extension methods for <see cref="PropertyBag"/>.
+    /// </summary>
+    public static class PropertyBagExtensions
+    {
+        /// <summary>
+        /// Get the property bag as a <see cref="IDictionary{TKey, TValue}"/> of <see cref="string"/> to <see cref="object"/>.
+        /// </summary>
+        /// <param name="propertyBag">The property bag.</param>
+        /// <returns>The property bag as a dictionary of objects (which can be cast to <see cref="JToken"/>).</returns>
+        public static IDictionary<string, object> AsDictionary(this PropertyBag propertyBag)
+        {
+            return propertyBag.Properties.Properties().ToDictionary(k => k.Name, v => (object)v.Value);
+        }
+    }
+}
